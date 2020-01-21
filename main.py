1b273e408fcc8271c1a5301fc97ceba4a545f196
Urafkard <ls1811589@gmail.com>
2020-01-21 10:58:13 +0000

    -adicionar comentarios

--- main.py
+++ main.py
@@ -12,21 +12,20 @@ import random
 
 from time import time
 
-#from itertools import product
-import itertools
-#import numpy as np
 
 
-#password maker
-# Write your program here
 
-symbolminus = 4 #black
-symbolplus = 3  #green
+#relação de cores com o simbolos
 symbolcross = 1 #red
-symbolcircle = 2    #blue
+symbolcircle = 2 #blue
+symbolplus = 3  #green
+symbolminus = 4 #black
 
 
-heur = 2
+
+
+
+
 
 #heuristica 1 espaços
 mapheur1 = [[symbolminus, symbolminus, symbolcross,None,       symbolcross],
@@ -48,6 +47,9 @@ pecasExistentesOld = []
 
 pecasRequeridasMax = (9,-1,9,3)
 pecasRequeridasMin = (5,4,5,2)
+
+
+
 
 def product(*args, repeat=1):
     # product('ABCD', 'xy') --> Ax Ay Bx By Cx Cy Dx Dy
@@ -75,7 +77,6 @@ def sortF(t,symbol,rank):
 def sortBySymbol(symbol):
   global allPoints
   rank = getPriorityRank(symbol)
-  #print("rank: " + str(rank))
   allPoints = sorted(allPoints, key=lambda t: sortF(t,symbol,rank), reverse=False)
 
 score = 0
@@ -83,7 +84,6 @@ score = 0
 
 
 def getPriorityRank(symbol):
-  #print(str(symbol), "peças: ", pecasExistentes[symbol-1] )
   if(pecasExistentes[symbol-1]/pecasRequeridasMax[symbol-1]>=1 and symbol != 2):
     return 2
   elif(pecasExistentes[symbol-1]/pecasRequeridasMin[symbol-1]>=1):
@@ -159,7 +159,7 @@ figureSequence = ("bcircle","bcross","bplus","bminus","mcircle2","scross","splus
 symbols = (" ","x","o","+","-")
 
 
-
+#função desenha a matrix
 def printMatrix(matrix):
     text = "-----\n"
     for x in range(len(matrix)):
@@ -174,42 +174,38 @@ def printMatrix(matrix):
 
 
 
-
+#função para encontrar padrão numa matrix
 def match_pattern(input_array, pattern, wildcard_function=None):
-    #print(input_array)
-    #print(pattern)
+
     pattern_shape = (len(pattern),len(pattern[0]))
     input_shape = (len(input_array),len(input_array[0]))
 
-
+    #verifica se as duas matrizes tem as mesmas dimensões
     if len(pattern_shape) != len(input_shape):
         raise ValueError("Input array and pattern must have the same dimension")
 
     shape_difference = [i_s - p_s for i_s, p_s in zip(input_shape, pattern_shape)]
 
+    #verifica se o comprimento da matrix e superior ao do padrão
     if any((diff < -1 for diff in shape_difference)):
         raise ValueError("Input array cannot be smaller than pattern in any dimension")
 
     dimension_iterators = [range(0, s_diff + 1) for s_diff in shape_difference]
 
-    # This loop will iterate over every possible "window" given the shape of the pattern
+    # iteração sobre todas as possiveis possições do padrão na matrix
     for start_indexes in product(*dimension_iterators):
-        #print(start_indexes,pattern_shape,zip(start_indexes, pattern_shape))
-        #for start_i, p_s in zip(start_indexes, pattern_shape):
-            #print(start_i, p_s)
+
         range_indexes = [(start_i, start_i + p_s) for start_i, p_s in zip(start_indexes, pattern_shape)]
-        #print(range_indexes)
+
         input_match_candidate = [[0 for x in range(range_indexes[1][0],range_indexes[1][1])] for y in range(range_indexes[0][0],range_indexes[0][1])]
-        #print(input_match_candidate)
+        
         for x in range(len(input_match_candidate)):
             xInput = range_indexes[0][0]+x
             for y in range(len(input_match_candidate[0])):
                 yInput = range_indexes[1][0]+y
                 input_match_candidate[x][y] = input_array[xInput][yInput]
-        #print(input_match_candidate)
-        #print(input_match_candidate)
-        # This checks that for the current "window" - the candidate - every element is equal 
-        #  to the pattern OR the element in the pattern is a wildcard
+
+        #verifica se todos os valores coincidem ou são a wildcard do padrão na matrix
         correct = True
         for x in range(len(input_match_candidate)):
             for y in range(len(input_match_candidate[x])):
@@ -225,9 +221,9 @@ colors = ("No Color", "Red", "Blue", "Green", "Black", "Yellow")
 
 VOLUME = 100
 
-
+#classe do robot
 class robot:
-    #status Variables
+    #variaveis principais
     position = [0,0]
     Clawdir = -1
     lista_lobby = []
@@ -250,6 +246,7 @@ class robot:
     x_motor2 = None
     xAxisMotors = None
 
+    #função de inicialização do robo
     def __init__(self):
         self.resetY_Sensor = TouchSensor(Port.S2)
         self.chromoSensor = ColorSensor(Port.S1)
@@ -267,32 +264,39 @@ class robot:
 
         self.reset()
 
+    #metodo para resetar a garra
     def reset_Claw(self):
         self.claw_Motor.run_until_stalled(1000,Stop.BRAKE,38)
         self.claw_Motor.run_time(5500*-1, 1700,Stop.BRAKE)
         self.Clawdir = -1
 
+    #metodo para resetar o eixo do Y
     def reset_YAxis(self):
         self.y_Motor.run(300)
         while (self.resetY_Sensor.pressed()== False):
             wait(10)
         self.y_Motor.stop()
 
+    #metodo para combinar ambos os resets anteriores
     def reset(self):
         self.reset_Claw()
         self.reset_YAxis()
         print("done reset")
 
+    #metodo para abrir e fechar a garra
     def toggleClaw(self):
         self.claw_Motor.run_time(5500*self.Clawdir,1300,Stop.BRAKE)
         self.Clawdir = -self.Clawdir
 
+    #metodo para o movimento no eixo dos Y
     def yAxis(self,pos):
         self.y_Motor.run_angle(100,-217.7*pos,Stop.BRAKE,True)
 
+    #metodo para o movimento no eixo dos X
     def xAxis(self,pos,dir):
         self.xAxisMotors.drive_time(dir*18.75, 0, 1000*pos*4)
     
+    #combinação de ambos os metodos anteriores
     def moveTo(self,xPos,yPos,resetY=True):
         dir = 1
         if(resetY):
@@ -307,16 +311,19 @@ class robot:
         self.position[0]= xPos
         self.position[1]= yPos
     
+    #metodo para o movimento no lobby onde lê as peças
     def moveToLobby(self,pos,resetY = True):
         if(pos%5==0):
             resetY = True
         self.moveTo(pos//5+5,pos%5,resetY)
 
+    #metodo para o movimento dentro do campo de jogo
     def moveToGame(self,xPos,yPos):
         if(xPos>4 or yPos>4):
             quit()
         self.moveTo(xPos,yPos,True)
 
+    #metodo para ler a cor repitindo 10 vezes
     def readColor(self):
         while(True):
             tempList = []
@@ -329,6 +336,25 @@ class robot:
                 if(tempList[0]!=0):
                     return tempList[0]
 
+    #metodo para testar a cor individualmente
+    def testecor(self,a):
+        #print(a)
+        if(a[0]>a[1]+a[2] and a[0]+a[1]+a[2]>25):
+            return 1 #vermelho
+        elif(a[2]> a[0]+a[1] and a[0]+a[1]+a[2]>25):
+            return 2 #azul
+        elif(a[0]+a[1]+a[2]<70):
+            temp = self.chromoSensor.color()
+            if(temp==3):
+                return 3 #verde
+            elif(temp==1):
+                return 4 #preto
+            else:
+                return 0 #sem cor
+        elif(a[0]+a[1]+a[2]<180):
+            return 5 #amarelo
+
+    #metodo que combina o movimento e a leitura de cores para ler todo o lobby
     def readLobby(self):
         pos = 0
         self.moveToLobby(pos,True)
@@ -351,29 +377,14 @@ class robot:
         pecasExistentesOld = pecasExistentes.copy()
         print(self.lista_lobby)
 
-    def testecor(self,a):
-        #print(a)
-        if(a[0]>a[1]+a[2] and a[0]+a[1]+a[2]>25):
-            return 1 #vermelho
-        elif(a[2]> a[0]+a[1] and a[0]+a[1]+a[2]>25):
-            return 2 #azul
-        elif(a[0]+a[1]+a[2]<70):
-            temp = self.chromoSensor.color()
-            if(temp==3):
-                return 3 #verde
-            elif(temp==1):
-                return 4 #preto
-            else:
-                return 0 #sem cor
-        elif(a[0]+a[1]+a[2]<180):
-            return 5 #amarelo
-    
+    #metodo que verifica se uma figura foi feita
     def checkFigure(self):
+        #itera sobre todos os padrões e verifica se existe algum
         for pattern in figureSequence:
             result = match_pattern(self.matrix,globals()[pattern])
-            #print(result)
             print(self.matrix)
             if(result != -1):
+                globals()["score"] += globals()[pattern + "p"]
                 for x in range(len(result[1])):
                     for y in range(len(result[1][x])):
                         if(result[1][x][y]==self.matrix[x+result[0][0]][y+result[0][1]]):
@@ -386,7 +397,8 @@ class robot:
                     if Button.CENTER in brick.buttons():
                         block = False
                     wait(10)
-    
+
+    #metodo que combina os metodos anteriores para mover uma peça para uma posição especifica
     def movePieceTo(self, xPos, yPos):
         self.moveToLobby(self.posicao_lobby)
         self.matrix[xPos][yPos] = self.lista_lobby[self.posicao_lobby]
@@ -403,20 +415,33 @@ class robot:
     
     
 
+heur = 1
 
 brick.sound.beep()
 
 
+#seleção da heuristica
+block = True
+brick.display.text("direita heur 1", (0, 20))
+brick.display.text("esquerda heur 2", (0, 40))
+while(block):
+    if Button.RIGHT in brick.buttons():
+        heur = 1
+        block = False
+    if Button.LEFT in brick.buttons():
+        heur = 2
+        block = False
+    wait(10)
 
 
+#inicialização do robo
 random.seed(int(time()))
 clangy = robot()
 
-#print(match_pattern(clangy.matrix, globals()["sminus"]))
-#clangy.checkFigure()
-#clangy.reset_Claw()
+
 clangy.readLobby()
 
+#listagem das figuras
 for color in clangy.lista_lobby:
    wait(10)
    if color == 1:
@@ -430,16 +455,14 @@ for color in clangy.lista_lobby:
    if color == 5:
        brick.sound.file(SoundFile.YELLOW,VOLUME)
 
-#clangy.moveToGame(0,0)
 
-# while(block):
-#     if Button.CENTER in brick.buttons():
-#         block = False
-#     wait(10)
+
+
+#execução da heuristica 1
 if(heur==1):
-    print("hello?")
     print(clangy.lista_lobby)
     for i in range(len(clangy.lista_lobby)):
+        #verifica se esta na altura de o robo fazer reset
         if(i%5 == 0):
             clangy.moveToGame(0,0)
             brick.sound.file(SoundFile.HELLO)
@@ -450,21 +473,22 @@ if(heur==1):
                 wait(10)
             wait(500)
         found = False
+        #procura de um expaço vazio para colocar a figura
         for mapX in range(len(mapheur1)):
             for mapY in range(len(mapheur1[0])):
                 if(mapheur1[mapX][mapY] == clangy.lista_lobby[clangy.posicao_lobby] and clangy.matrix[mapX][mapY] == 0):
                     found = True
-                    #print(pecasExistentes)
                     clangy.movePieceTo(mapX,mapY)
                     break
             if(found):
                 break
 
 
-
+#execução da heuristica 2 
 if(heur == 2):
     for i in range(len(clangy.lista_lobby)):
-        found = False
+        
+        #verifica se esta na altura de o robo fazer reset
         if(i%5 == 0):
             clangy.moveToGame(0,0)
             brick.sound.file(SoundFile.HELLO)
@@ -474,6 +498,8 @@ if(heur == 2):
                     block = False
                 wait(10)
             wait(500)
+        found = False
+        #encontrar a posição para colocar a proxima peça
         sortBySymbol(clangy.lista_lobby[clangy.posicao_lobby])
         for x in allPoints:
             if(clangy.matrix[x[0]][x[1]] == 0):
@@ -482,77 +508,11 @@ if(heur == 2):
                 found = True
                 break
 
+brick.display.clear()
+brick.display.text(score, (0, 20))
 
-
-
-#for i in range(len(clangy.lista_lobby)):
-# #for i in range(2):
-#    getting = True
-#    xPos = 0
-#    yPos = 0
-#    if(i%5 == 0):
-#        clangy.moveToGame(0,0)
-#        brick.sound.file(SoundFile.HELLO)
-#        block = True
-#        while(block):
-#            if Button.CENTER in brick.buttons():
-#                block = False
-#            wait(10)
-#        wait(500)
-
-#    while(getting):
-#       xPos = random.randint(0,4)
-#       yPos = random.randint(0,4)
-#       if(clangy.matrix[xPos][yPos] == 0):
-#           getting = False
-#    clangy.movePieceTo(xPos,yPos)
-    #clangy.movePieceTo(clangy.posicao_lobby//5,clangy.posicao_lobby%5)
-
-
-#clangy.moveToLobby(0)
-#clangy.toggleClaw()
-#clangy.moveToGame(1,3)
-#clangy.toggleClaw()
-#for x in range(10):
-#    positionGame = [random.randint(0,4),random.randint(0,4)]
-#    print(positionGame)
-#    clangy.moveToGame(positionGame[0],positionGame[1])
-#    positionLobby = random.randint(0,10)
-#    print(positionLobby)
-#    clangy.moveToLobby(positionLobby)
-
-
-
-
-    #.run_time(250,2000,Stop.BRAKE)
-
-
-
-
-    
-    #xAxisMotors.run_angle(200,-270*pos,Stop.BRAKE,True)
-    #x_Motor1.run_angle(200,-270*pos,Stop.BRAKE,False)
-    #x_Motor2.run_angle(200,-270*pos,Stop.BRAKE,True)
-
-#reset()
-#yAxis()
-#toggleClaw()
-#yAxis(0.5)
-#toggleClaw()
-#xAxis(4)
-#xAxis(5,-1)
-#toggleClaw()
-#
-#toggleClaw()
-#while(1):
-#    temp = readColor()
-#    if(temp != None):
-#        brick.display.clear()
-#        brick.display.text(temp, (0, 20))
-#    wait(1000)
-
-
-    
-#for x in range(20):
-#    xAxis(1)
-#    xAxis(-1)
+while(block):
+    if Button.CENTER in brick.buttons():
+        block = False
+    wait(10)
+wait(500)
